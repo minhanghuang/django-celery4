c426afe6711b78fbce16996799ebe9d25784ae44
coxhuang <mhesat@outlook.com>
2019-05-02 17:35:43 +0800

    update

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from app.tasks import send_email
 
 def email_api(request):
 
-    send_email.delay() # 发送邮件
+    result = send_email.delay() # 发送邮件
+    result.get()
 
     return HttpResponse("邮件发送成功 !!! ")
